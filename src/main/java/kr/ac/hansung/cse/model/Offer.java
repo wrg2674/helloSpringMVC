5efd7e15bf8859ef9309f9444221d5eaec65a31e
wrg2674 <wrg267499@naver.com>
2024-03-30 23:41:48 +0900

    add spring web form functionality

--- src/main/java/kr/ac/hansung/cse/model/Offer.java
+++ src/main/java/kr/ac/hansung/cse/model/Offer.java
@@ -5,13 +5,22 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class Offer {
     private int id;
+    @Size(min=2, max=100, message="Name must be between 2 and 100 chars")
     private String name;
+
+    @NotEmpty(message="The email address cannot be empty")
+    @Email(message="Please provide a valid email address")
     private String email;
+
+    @Size(min=5, max=100, message="Text must be between 5 and 100 chars")
     private String text;
 }
---
+++ src/main/webapp/WEB-INF/views/offercreated.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 정구현
+  Date: 2024-03-30
+  Time: 오후 10:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${offer.name} 가 새로운 제안을 하였습니다.
+<a href = "${pageContext.request.contextPath}/offers">Click here to view current offers</a>
+</body>
+</html>
